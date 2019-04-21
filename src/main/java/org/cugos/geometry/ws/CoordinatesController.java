176ee672558d67a6d36182cd4aaed7493001dd9f
jericks <jared.erickson@gmail.com>
2019-04-21 09:56:18 -0700

    Add coordinates web service

---
+++ src/main/java/org/cugos/geometry/ws/CoordinatesController.java
@@ -0,0 +1,53 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.util.UniqueCoordinateArrayFilter;
+
+@Controller("/coordinates")
+public class CoordinatesController {
+  
+  @Get("/{from}/{to}")
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Get Coordinates", description = "Get the coordinates of the geometry.")
+  public String get(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @QueryValue("geom") String geometryString,
+      @Parameter(description = "Only include unique coordinates") @QueryValue("unique") Boolean unique) throws Exception {
+    return coordinates(from, to, geometryString, unique == null ? Boolean.FALSE : unique);
+  }
+
+  @Post("/{from}/{to}")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Get Coordinates", description = "Get the coordinates of the geometry.")
+  public String post(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @Body String geometryString,
+      @Parameter(description = "Only include unique coordinates") @QueryValue("unique") Boolean unique) throws Exception {
+     return coordinates(from, to, geometryString, unique == null ? Boolean.FALSE : unique);
+  }
+
+  private String coordinates(String from, String to, String geometryString, boolean isUnique) throws Exception {
+    GeometryReader reader = GeometryReaders.find(from);
+    GeometryWriter writer = GeometryWriters.find(to);
+    Geometry geometry = reader.read(geometryString);
+
+    Coordinate[] coords = geometry.getCoordinates();
+    Geometry outputGeometry = geometry.getFactory().createMultiPointFromCoords(coords);
+    if (isUnique)  {
+      UniqueCoordinateArrayFilter coordinateFilter = new UniqueCoordinateArrayFilter();
+      outputGeometry.apply(coordinateFilter);
+      outputGeometry = geometry.getFactory().createMultiPoint(coordinateFilter.getCoordinates());
+    }
+
+    return writer.write(outputGeometry);
+  }
+
+}
---
+++ src/test/java/org/cugos/geometry/ws/CoordinatesControllerTest.java
@@ -0,0 +1,32 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+
+public class CoordinatesControllerTest extends AbstractControllerTest  {
+
+    private final String polygon = "POLYGON ((-126.91406249999999 44.08758502824516, -116.3671875 44.08758502824516, -116.3671875 50.736455137010665, -126.91406249999999 50.736455137010665, -126.91406249999999 44.08758502824516))";
+
+    @Test
+    public void get() throws Exception {
+        HttpRequest request = HttpRequest.GET("/coordinates/wkt/wkt?unique=true&geom=" + URLEncoder.encode(polygon, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("MULTIPOINT ((-126.91406249999999 44.08758502824516), (-116.3671875 44.08758502824516), " +
+            "(-116.3671875 50.736455137010665), (-126.91406249999999 50.736455137010665))", geometry);
+    }
+    
+    @Test
+    public void post() throws Exception {
+        HttpRequest request = HttpRequest.POST("/coordinates/wkt/wkt?unique=false", polygon).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("MULTIPOINT ((-126.91406249999999 44.08758502824516), (-116.3671875 44.08758502824516), " +
+            "(-116.3671875 50.736455137010665), (-126.91406249999999 50.736455137010665), " +
+            "(-126.91406249999999 44.08758502824516))", geometry);
+    }
+
+}
