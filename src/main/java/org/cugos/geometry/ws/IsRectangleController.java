d70873fb606394ad067637340d68365dcb5e32f0
jericks <jared.erickson@gmail.com>
2019-05-06 17:59:08 -0700

    Add is rectangle web service

---
+++ src/main/java/org/cugos/geometry/ws/IsRectangleController.java
@@ -0,0 +1,38 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.locationtech.jts.geom.Geometry;
+
+@Controller("/isRectangle")
+public class IsRectangleController {
+  
+  @Get("/{from}")
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Is Rectangle", description = "Determine if the input geometry is a rectangle or not.")
+  public String get(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Input Geometry") @QueryValue("geom") String geometryString) throws Exception {
+    return isRectangle(from, geometryString);
+  }
+
+  @Post("/{from}")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Is Rectangle", description = "Determine if the input geometry is a rectangle or not.")
+  public String post(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Input Geometry") @Body String geometryString) throws Exception {
+     return isRectangle(from, geometryString);
+  }
+
+  private String isRectangle(String from, String geometryString) throws Exception {
+    GeometryReader reader = GeometryReaders.find(from);
+    Geometry geometry = reader.read(geometryString);
+    boolean value = geometry.isRectangle();
+    return String.valueOf(value);
+  }
+
+}
---
+++ src/test/java/org/cugos/geometry/ws/IsRectangleControllerTest.java
@@ -0,0 +1,45 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+
+public class IsRectangleControllerTest extends AbstractControllerTest  {
+
+    private final String rectangularGeometry = "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))";
+
+    private final String nonRectangularGeometry = "POLYGON ((0 0, 0 10, 10 0, 0 0))";
+
+    @Test
+    public void getNotRectangle() throws Exception {
+        HttpRequest request = HttpRequest.GET("/isRectangle/wkt?geom=" + URLEncoder.encode(nonRectangularGeometry, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("false", geometry);
+    }
+
+    @Test
+    public void getIsRectangle() throws Exception {
+        HttpRequest request = HttpRequest.GET("/isRectangle/wkt?geom=" + URLEncoder.encode(rectangularGeometry, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("true", geometry);
+    }
+    
+    @Test
+    public void postNotRectangle() throws Exception {
+        HttpRequest request = HttpRequest.POST("/isRectangle/wkt", nonRectangularGeometry).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("false", geometry);
+    }
+
+    @Test
+    public void postRectangle() throws Exception {
+        HttpRequest request = HttpRequest.POST("/isRectangle/wkt", rectangularGeometry).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("true", geometry);
+    }
+
+}
