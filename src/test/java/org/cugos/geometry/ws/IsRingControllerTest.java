c40c3390f0073eba74251618a834337026f72422
jericks <jared.erickson@gmail.com>
2019-05-09 17:05:41 -0700

    Add is ring web service

---
+++ src/test/java/org/cugos/geometry/ws/IsRingControllerTest.java
@@ -0,0 +1,45 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+
+public class IsRingControllerTest extends AbstractControllerTest  {
+
+    private final String isRingGeometry = "LINESTRING (1 1, 1 5, 5 5, 5 1, 1 1)";
+
+    private final String isNotRingGeometry = "LINESTRING (1 1, 5 5, 10 10)";
+
+    @Test
+    public void getIsNotRing() throws Exception {
+        HttpRequest request = HttpRequest.GET("/isRing/wkt?geom=" + URLEncoder.encode(isNotRingGeometry, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("false", geometry);
+    }
+
+    @Test
+    public void getIsRing() throws Exception {
+        HttpRequest request = HttpRequest.GET("/isRing/wkt?geom=" + URLEncoder.encode(isRingGeometry, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("true", geometry);
+    }
+    
+    @Test
+    public void postIsNotRing() throws Exception {
+        HttpRequest request = HttpRequest.POST("/isRing/wkt", isNotRingGeometry).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("false", geometry);
+    }
+
+    @Test
+    public void postIsRectangle() throws Exception {
+        HttpRequest request = HttpRequest.POST("/isRing/wkt", isRingGeometry).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("true", geometry);
+    }
+
+}
