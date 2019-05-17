7b2dee150fc6a4154f6e2b82a8a21e622847d820
jericks <jared.erickson@gmail.com>
2019-05-17 16:07:49 -0700

    Add minimum bounding circle web service

---
+++ src/main/java/org/cugos/geometry/ws/MinimumBoundingCircleController.java
@@ -0,0 +1,43 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.locationtech.jts.algorithm.MinimumBoundingCircle;
+import org.locationtech.jts.geom.Geometry;
+
+@Controller("/minimumBoundingCircle")
+public class MinimumBoundingCircleController {
+  
+  @Get("/{from}/{to}")
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Calculate Minimum Bounding Circle", description = "Calculate Minimum Bounding Circle of a Geometry")
+  public String get(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @QueryValue("geom") String geometryString) throws Exception {
+    return minimumBoundingCircle(from, to, geometryString);
+  }
+
+  @Post("/{from}/{to}")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Calculate Minimum Bounding Circle", description = "Calculate Minimum Bounding Circle of a Geometry")
+  public String post(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @Body String geometryString) throws Exception {
+     return minimumBoundingCircle(from, to, geometryString);
+  }
+
+  private String minimumBoundingCircle(String from, String to, String geometryString) throws Exception {
+    GeometryReader reader = GeometryReaders.find(from);
+    GeometryWriter writer = GeometryWriters.find(to);
+    Geometry geometry = reader.read(geometryString);
+    MinimumBoundingCircle circle = new MinimumBoundingCircle(geometry);
+    Geometry outputGeometry = circle.getCircle();
+    return writer.write(outputGeometry);
+  }
+
+}
