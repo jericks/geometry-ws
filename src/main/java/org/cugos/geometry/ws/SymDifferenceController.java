eb2e969e43b811e99cb733139e5abb1abe79af9d
jericks <jared.erickson@gmail.com>
2019-06-24 17:16:38 -0700

    Add symetric difference web service

---
+++ src/main/java/org/cugos/geometry/ws/SymDifferenceController.java
@@ -0,0 +1,48 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.locationtech.jts.geom.Geometry;
+
+@Controller("/symDifference")
+public class SymDifferenceController {
+  
+  @Get("/{from}/{to}")
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "SymDifference", description = "Calculate the symetric symDifference between two geometries")
+  public HttpResponse get(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @QueryValue("geom") String geometryString) throws Exception {
+    return symDifference(from, to, geometryString);
+  }
+
+  @Post("/{from}/{to}")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "SymDifference", description = "Calculate the symetric symDifference between two geometries")
+  public HttpResponse post(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Output Geometry Format (wkt, geojson, kml, gml2)") String to,
+      @Parameter(description = "Input Geometry") @Body("geom") String geometryString) throws Exception {
+    return symDifference(from, to, geometryString);
+  }
+
+  private HttpResponse symDifference(String from, String to, String geometryString) throws Exception {
+    GeometryReader reader = GeometryReaders.find(from);
+    GeometryWriter writer = GeometryWriters.find(to);
+    Geometry geometry = reader.read(geometryString);
+    if (Validator.hasRequiredNumberOfGeometries(geometry, 2)) {
+      Geometry firstGeometry = geometry.getGeometryN(0);
+      Geometry secondGeometry = geometry.getGeometryN(1);
+      Geometry result = firstGeometry.symDifference(secondGeometry);
+      return HttpResponse.ok(result).contentType(new MediaType(writer.getMediaType()));
+    } else {
+      return HttpResponse.badRequest("A Geometry Collection with two geometries is required!");
+    }
+  }
+
+}
---
+++ src/test/java/org/cugos/geometry/ws/SymDifferenceControllerTest.java
@@ -0,0 +1,46 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.exceptions.HttpClientException;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+
+public class SymDifferenceControllerTest extends AbstractControllerTest  {
+
+    private final String polygonGeometry = "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))";
+    private final String anotherPolygonGeometry = "POLYGON ((5 5, 5 20, 20 20, 20 5, 5 5))";
+    private final String symDifferenceGeometry = "MULTIPOLYGON (((0 0, 0 10, 5 10, 5 5, 10 5, 10 0, 0 0)), " +
+        "((10 5, 10 10, 5 10, 5 20, 20 20, 20 5, 10 5)))";
+    private final String pointGeometry = "POINT (5 5)";
+
+    private String geometryCollection(String firstGeometry, String secondGeometry) {
+        return String.format("GEOMETRYCOLLECTION (%s, %s)", firstGeometry, secondGeometry);
+    }
+
+    @Test
+    public void get() throws Exception {
+        HttpRequest request = HttpRequest.GET("/symDifference/wkt/wkt" +
+            "?geom=" + URLEncoder.encode(geometryCollection(polygonGeometry, anotherPolygonGeometry), "UTF-8"));
+        String value = client.toBlocking().retrieve(request);
+        assertEquals(symDifferenceGeometry, value);
+    }
+
+    @Test
+    public void post() throws Exception {
+        HttpRequest request = HttpRequest.POST("/symDifference/wkt/wkt", geometryCollection(polygonGeometry, anotherPolygonGeometry))
+            .contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals(symDifferenceGeometry, geometry);
+    }
+
+    @Test(expected = HttpClientException.class)
+    public void badRequest() throws Exception {
+        HttpRequest request = HttpRequest.GET("/difference/wkt/wkt?geom=" + URLEncoder.encode(pointGeometry, "UTF-8"));
+        client.toBlocking().retrieve(request);
+    }
+
+}
