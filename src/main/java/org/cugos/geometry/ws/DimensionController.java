4b0119fb064addcac481fd413572503f1395fae3
jericks <jared.erickson@gmail.com>
2019-08-25 15:48:35 -0700

    Add dimension web service

---
+++ src/main/java/org/cugos/geometry/ws/DimensionController.java
@@ -0,0 +1,38 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.locationtech.jts.geom.Geometry;
+
+@Controller("/dimension")
+public class DimensionController {
+  
+  @Get("/{from}")
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Get Dimension", description = "Get the dimension of a Geometry")
+  public String get(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Input Geometry") @QueryValue("geom") String geometryString) throws Exception {
+    return dimension(from, geometryString);
+  }
+
+  @Post("/{from}")
+  @Consumes(MediaType.TEXT_PLAIN)
+  @Produces(MediaType.TEXT_PLAIN)
+  @Operation(summary = "Get Dimension", description = "Get the dimension of a Geometry")
+  public String post(
+      @Parameter(description = "Input Geometry Format (wkt, geojson, kml, gml2)")  String from,
+      @Parameter(description = "Input Geometry") @Body String geometryString) throws Exception {
+     return dimension(from, geometryString);
+  }
+
+  private String dimension(String from, String geometryString) throws Exception {
+    GeometryReader reader = GeometryReaders.find(from);
+    Geometry geometry = reader.read(geometryString);
+    int value = geometry.getDimension();
+    return String.valueOf(value);
+  }
+
+}
---
+++ src/test/java/org/cugos/geometry/ws/DimensionControllerTest.java
@@ -0,0 +1,29 @@
+package org.cugos.geometry.ws;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+
+import static org.junit.Assert.assertEquals;
+
+public class DimensionControllerTest extends AbstractControllerTest  {
+
+    private final String polygon = "POLYGON ((-126.91406249999999 44.08758502824516, -116.3671875 44.08758502824516, -116.3671875 50.736455137010665, -126.91406249999999 50.736455137010665, -126.91406249999999 44.08758502824516))";
+
+    @Test
+    public void get() throws Exception {
+        HttpRequest request = HttpRequest.GET("/dimension/wkt?geom=" + URLEncoder.encode(polygon, "UTF-8"));
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("2", geometry);
+    }
+    
+    @Test
+    public void post() throws Exception {
+        HttpRequest request = HttpRequest.POST("/dimension/wkt", polygon).contentType(MediaType.TEXT_PLAIN_TYPE);
+        String geometry = client.toBlocking().retrieve(request);
+        assertEquals("2", geometry);
+    }
+
+}
